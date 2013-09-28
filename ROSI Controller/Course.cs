d5db5f1eae265abe697e19e4a2398c74da619ecb
Allen Hsiao <allenhsiao0728@hotmail.com>
2013-09-28 14:14:09 -0400

    Added basic course information class

---
+++ ROSI Controller/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROSI_Controller
+{
+    class Course
+    {
+        public string courseCode 
+        {
+            get { return courseCode; }
+            set { this.courseCode = value; }
+
+        }
+        public string sectionCode
+        {
+            get { return sectionCode; }
+            set { this.sectionCode = value; }
+        }
+
+    }
+}
