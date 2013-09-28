ef3dfe7fd2f25958ab17608a98e40d0ca608af48
Allen Hsiao <allenhsiao0728@hotmail.com>
2013-09-28 14:16:24 -0400

    Added tab control group for selecting operation types

--- ROSI Controller/Form1.Designer.cs
+++ ROSI Controller/Form1.Designer.cs
@@ -36,8 +36,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.operation = new System.Windows.Forms.TabPage();
+            this.ops = new System.Windows.Forms.TabControl();
+            this.autoEnrolmentTab = new System.Windows.Forms.TabPage();
+            this.singleCourseButton = new System.Windows.Forms.Button();
+            this.singleSectionCodeTextbox = new System.Windows.Forms.TextBox();
+            this.singleCourseCodeTextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.general.SuspendLayout();
             this.auth.SuspendLayout();
+            this.operation.SuspendLayout();
+            this.ops.SuspendLayout();
+            this.autoEnrolmentTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // general
@@ -47,7 +58,7 @@
             this.general.Location = new System.Drawing.Point(3, -3);
             this.general.Name = "general";
             this.general.SelectedIndex = 0;
-            this.general.Size = new System.Drawing.Size(607, 363);
+            this.general.Size = new System.Drawing.Size(495, 306);
             this.general.TabIndex = 21;
             // 
             // auth
@@ -60,14 +71,14 @@
             this.auth.Location = new System.Drawing.Point(4, 22);
             this.auth.Name = "auth";
             this.auth.Padding = new System.Windows.Forms.Padding(3);
-            this.auth.Size = new System.Drawing.Size(599, 337);
+            this.auth.Size = new System.Drawing.Size(487, 280);
             this.auth.TabIndex = 0;
             this.auth.Text = "tabPage1";
             this.auth.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(257, 195);
+            this.button1.Location = new System.Drawing.Point(208, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 25;
@@ -77,14 +88,14 @@
             // 
             // pinText
             // 
-            this.pinText.Location = new System.Drawing.Point(288, 156);
+            this.pinText.Location = new System.Drawing.Point(239, 127);
             this.pinText.Name = "pinText";
             this.pinText.Size = new System.Drawing.Size(100, 20);
             this.pinText.TabIndex = 24;
             // 
             // sNumText
             // 
-            this.sNumText.Location = new System.Drawing.Point(288, 119);
+            this.sNumText.Location = new System.Drawing.Point(239, 90);
             this.sNumText.Name = "sNumText";
             this.sNumText.Size = new System.Drawing.Size(100, 20);
             this.sNumText.TabIndex = 23;
@@ -92,7 +103,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(249, 159);
+            this.label2.Location = new System.Drawing.Point(200, 130);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 13);
             this.label2.TabIndex = 22;
@@ -101,7 +112,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(187, 119);
+            this.label1.Location = new System.Drawing.Point(138, 90);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 21;
@@ -109,25 +120,108 @@
             // 
             // operation
             // 
+            this.operation.Controls.Add(this.ops);
             this.operation.Location = new System.Drawing.Point(4, 22);
             this.operation.Name = "operation";
             this.operation.Padding = new System.Windows.Forms.Padding(3);
-            this.operation.Size = new System.Drawing.Size(599, 337);
+            this.operation.Size = new System.Drawing.Size(487, 280);
             this.operation.TabIndex = 1;
             this.operation.Text = "tabPage2";
             this.operation.UseVisualStyleBackColor = true;
+            // 
+            // ops
+            // 
+            this.ops.Controls.Add(this.autoEnrolmentTab);
+            this.ops.Controls.Add(this.tabPage2);
+            this.ops.Location = new System.Drawing.Point(6, 6);
+            this.ops.Name = "ops";
+            this.ops.SelectedIndex = 0;
+            this.ops.Size = new System.Drawing.Size(475, 263);
+            this.ops.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.ops.TabIndex = 0;
+            // 
+            // autoEnrolmentTab
+            // 
+            this.autoEnrolmentTab.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.autoEnrolmentTab.Controls.Add(this.singleCourseButton);
+            this.autoEnrolmentTab.Controls.Add(this.singleSectionCodeTextbox);
+            this.autoEnrolmentTab.Controls.Add(this.singleCourseCodeTextbox);
+            this.autoEnrolmentTab.Controls.Add(this.label4);
+            this.autoEnrolmentTab.Controls.Add(this.label3);
+            this.autoEnrolmentTab.Location = new System.Drawing.Point(4, 22);
+            this.autoEnrolmentTab.Name = "autoEnrolmentTab";
+            this.autoEnrolmentTab.Padding = new System.Windows.Forms.Padding(3);
+            this.autoEnrolmentTab.Size = new System.Drawing.Size(467, 237);
+            this.autoEnrolmentTab.TabIndex = 0;
+            this.autoEnrolmentTab.Text = "Automatic Single Course Enrolment";
+            // 
+            // singleCourseButton
+            // 
+            this.singleCourseButton.Location = new System.Drawing.Point(144, 137);
+            this.singleCourseButton.Name = "singleCourseButton";
+            this.singleCourseButton.Size = new System.Drawing.Size(175, 23);
+            this.singleCourseButton.TabIndex = 4;
+            this.singleCourseButton.Text = "Confirm";
+            this.singleCourseButton.UseVisualStyleBackColor = true;
+            this.singleCourseButton.Click += new System.EventHandler(this.singleCourseButton_Click);
+            // 
+            // singleSectionCodeTextbox
+            // 
+            this.singleSectionCodeTextbox.Location = new System.Drawing.Point(219, 96);
+            this.singleSectionCodeTextbox.Name = "singleSectionCodeTextbox";
+            this.singleSectionCodeTextbox.Size = new System.Drawing.Size(100, 20);
+            this.singleSectionCodeTextbox.TabIndex = 3;
+            // 
+            // singleCourseCodeTextbox
+            // 
+            this.singleCourseCodeTextbox.Location = new System.Drawing.Point(219, 59);
+            this.singleCourseCodeTextbox.Name = "singleCourseCodeTextbox";
+            this.singleCourseCodeTextbox.Size = new System.Drawing.Size(100, 20);
+            this.singleCourseCodeTextbox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Section Code:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Course Code:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(467, 237);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 372);
+            this.ClientSize = new System.Drawing.Size(497, 299);
             this.Controls.Add(this.general);
             this.Name = "Form1";
             this.Text = "Form1";
             this.general.ResumeLayout(false);
             this.auth.ResumeLayout(false);
             this.auth.PerformLayout();
+            this.operation.ResumeLayout(false);
+            this.ops.ResumeLayout(false);
+            this.autoEnrolmentTab.ResumeLayout(false);
+            this.autoEnrolmentTab.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -142,6 +236,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabPage operation;
+        private System.Windows.Forms.TabControl ops;
+        private System.Windows.Forms.TabPage autoEnrolmentTab;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox singleSectionCodeTextbox;
+        private System.Windows.Forms.TextBox singleCourseCodeTextbox;
+        private System.Windows.Forms.Button singleCourseButton;
 
 
 
