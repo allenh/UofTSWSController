8c3f7b1341f0121efc18887decf597a5929b7ea0
Allen Hsiao <allenhsiao0728@hotmail.com>
2013-09-28 01:48:00 -0400

    Basic authentication information form built

--- ROSI Controller/Form1.cs
+++ ROSI Controller/Form1.cs
@@ -12,9 +12,17 @@ namespace ROSI_Controller
 {
     public partial class Form1 : Form
     {
+        protected string studentNumber;
+        protected string pinNumber;
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            studentNumber = studentNum.Text;
+            pinNumber = pin.Text;
         }
     }
 }
